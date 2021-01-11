e327aa184b8a210f072b770185a1f255ae96a57b
kazuki-shimura <sim202025@icloud.com>
2021-01-12 00:20:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# FlutterでUIを学習するためのリポジトリ
+
+    このURLを写経
+    https://www.youtube.com/watch?v=ExKYjqgswJg
+
+    libファイルが多少異なる
+    ファイル名が多少異なる
+
+    1日目
+    1分55秒まで
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:ui_learning/Screens/welcome.dart';
+import 'package:ui_learning/constants.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
