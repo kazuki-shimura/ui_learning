4710803c845ade74e1110f0a19e300d2fdca0a1a
kazuki-shimura <sim202025@icloud.com>
2021-01-14 23:54:10 +0900

    error回避

--- lib/Screens/components/body.dart
+++ lib/Screens/components/body.dart
@@ -1,11 +1,12 @@
 import "package:flutter/material.dart";
 import "package:ui_learning/Screens/components/background.dart";
-// import "package:flutter_svg/svg.dart";
+import "package:flutter_svg/svg.dart";
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
     return Background(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -14,10 +15,10 @@ class Body extends StatelessWidget {
             "WELCOME TO EDU",
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          // SvgPicture.asset(
-          //   "assets/icons/chat.svg",
-          //   height: size.height * 0.45,
-          // ),
+          SvgPicture.asset(
+            "assets/icons/chat.svg",
+            height: size.height * 0.45,
+          ),
         ],
       ),
     );
